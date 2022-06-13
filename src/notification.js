694d24234139cea3fa403da16f4dcb435b3896bb
Christopher Pezza <chris.pezza@hey.com>
2022-06-12 22:06:21 -0400

    formating touch ups

--- src/notification.js
+++ src/notification.js
@@ -1,53 +1,53 @@
-import { Controller } from '@hotwired/stimulus';
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-	static targets = ['container'];
-	static classes = ['entering', 'enteringTo', 'enteringFrom', 'leaving', 'leavingTo', 'leavingFrom'];
-	static values = {
-		autoDismiss: { type: Boolean, default: false },
-		dismissAfter: Number,
-		enterTimeout: { type: Number, default: 1000 },
-		leaveTimeout: { type: Number, default: 1000 },
-	};
+  static targets = ['container']
+  static classes = ['entering', 'enteringTo', 'enteringFrom', 'leaving', 'leavingTo', 'leavingFrom']
+  static values = {
+    autoDismiss: { type: Boolean, default: false },
+    dismissAfter: Number,
+    enterTimeout: { type: Number, default: 1000 },
+    leaveTimeout: { type: Number, default: 1000 },
+  }
 
-	initialize() {
-		if (this.autoDismissValue) {
-			setTimeout(() => {
-				this.close();
-			}, this.dismissAfterValue);
-		}
-	}
+  initialize() {
+    if (this.autoDismissValue) {
+      setTimeout(() => {
+        this.close()
+      }, this.dismissAfterValue)
+    }
+  }
 
-	connected() {
-		setTimeout(() => {
-			this._show();
-		}, 1000);
-	}
+  connected() {
+    setTimeout(() => {
+      this._show()
+    }, 1000)
+  }
 
-	close() {
-		this._hide();
-		this.containerTarget.remove();
-	}
+  close() {
+    this._hide()
+    this.containerTarget.remove()
+  }
 
-	_show() {
-		this.containerTarget.classList.add(...this.enteringClasses);
+  _show() {
+    this.containerTarget.classList.add(...this.enteringClasses)
 
-		requestAnimationFrame(() => {
-			this.containerTarget.classList.add(...this.enteringToClasses);
-			this.containerTarget.classList.remove(...this.enteringFromClasses);
-			setTimeout(() => {
-				this.containerTarget.classList.remove(...this.enteringClasses);
-			}, this.enterTimeoutValue);
-		});
-	}
+    requestAnimationFrame(() => {
+      this.containerTarget.classList.add(...this.enteringToClasses)
+      this.containerTarget.classList.remove(...this.enteringFromClasses)
+      setTimeout(() => {
+        this.containerTarget.classList.remove(...this.enteringClasses)
+      }, this.enterTimeoutValue)
+    })
+  }
 
-	_hide() {
-		this.containerTarget.classList.add(...this.leavingToClasses);
-		this.containerTarget.classList.remove(...this.leavingFromClasses);
-		this.containerTarget.classList.add(...this.leavingClasses);
+  _hide() {
+    this.containerTarget.classList.add(...this.leavingToClasses)
+    this.containerTarget.classList.remove(...this.leavingFromClasses)
+    this.containerTarget.classList.add(...this.leavingClasses)
 
-		setTimeout(() => {
-			this.containerTarget.classList.remove(...this.leavingClasses);
-		}, this.leaveTimeoutValue);
-	}
+    setTimeout(() => {
+      this.containerTarget.classList.remove(...this.leavingClasses)
+    }, this.leaveTimeoutValue)
+  }
 }
