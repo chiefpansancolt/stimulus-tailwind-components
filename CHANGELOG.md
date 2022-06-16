d3e79e2f0d34a3169cb75833e0613f29776e91fd
Christopher Pezza <chris.pezza@hey.com>
2022-06-15 23:09:14 -0400

    Update base repo files

--- CHANGELOG.md
+++
@@ -1,8 +0,0 @@
-# Changelog
-
-## Version [0.0.2](https://github.com/chiefpansancolt/stimulus-tailwind-components/releases/tag/0.0.2)
-
-### Features
-
-- Notification(s) ([#2](https://github.com/chiefpansancolt/stimulus-tailwind-components/issues/2))
-  - Ability to Auto/Manual Dismissal
---
+++ esbuild-playground.js
@@ -0,0 +1,14 @@
+const esbuild = require('esbuild')
+
+esbuild
+  .build({
+    entryPoints: ['src/index.js'],
+    outfile: 'docs/dist/stimulus-tailwind-components.min.js',
+    bundle: true,
+    sourcemap: false,
+    minify: true,
+    splitting: false,
+    format: 'esm',
+    target: ['esnext'],
+  })
+  .catch(() => process.exit(1))
