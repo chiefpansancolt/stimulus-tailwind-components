108d4878b432496d827806ff3790ebcfc8369a93
Christopher Pezza <chris.pezza@hey.com>
2023-04-02 11:06:30 -0400

    update linters

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  semi: false,
+  singleQuote: false,
+  printWidth: 120,
+  trailingComma: "es5",
+  arrowParens: "always",
+  tabWidth: 2,
+  useTabs: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: "strict"
+}
